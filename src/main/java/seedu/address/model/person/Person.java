18eae0c36c2e9b312c395dbcb6ab3b1a29789378
poaoaooa <Ashutoshmenonrajeev@gmail.com>
2026-03-23 21:25:46 +0800

    Update Person.java

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -69,6 +69,8 @@ public class Person {
 
     public WeeklyAttendanceList getWeeklyAttendanceList() {
         return weeklyAttendanceList;
+    }
+    
     public String getRemarks() {
         return remarks;
     }
